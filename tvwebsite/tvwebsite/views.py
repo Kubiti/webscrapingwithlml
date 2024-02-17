43416b1cb97319bb3cef692046253e68f5ef8dd4
Samuel Kubiti <kubitisam18@gmail.com>
2024-02-17 18:50:31 +0000

    django-website

---
+++ tvwebsite/tvwebsite/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# our home page view
+def home(request):    
+    return render(request, 'index.html')
+
+
+# custom method for generating predictions
+def getPrediction(brand, size, disp, condition):
+    import pickle
+    import pandas as pd
+    model = pickle.load(open("model.sav", "rb"))
+
+    x_point = {'Brand': brand, 'Screen Size': size, 'Display Technology': disp, 'Condition': condition}
+    x_df = pd.DataFrame([x_point])
+
+    prediction = model.predict(x_df)
+    
+    return prediction
+
+
+
+# result page view
+def result(request):
+    brand = request.GET['brand']
+    size = int(request.GET['size'])
+    disp = request.GET['disp']
+    condition = request.GET['condition']
+
+    result = round(getPrediction(brand, size, disp, condition)[0], 2)
+    
+    return render(request, 'result.html', {'result': result})

ecc6182e37d80fe923d5b22afd6eb51e938109ea
Samuel Kubiti <kubitisam18@gmail.com>
2024-02-18 17:30:45 +0000

    allowed host

--- tvwebsite/tvwebsite/settings.py
+++ tvwebsite/tvwebsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-8gwc+-8o-a1%9sqasabr$&l6uw*lp!7$kyi2ml(^l1i1%o_!z=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tvmodel.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
